c6f74000eb3c40f151351691fce8e22c802315c3
Rui Niu <nrui@mail.ustc.edu.cn>
2023-11-20 18:13:47 +0800

    add bbhx in comparing and perf_cpu

--- sync.sh
+++ sync.sh
@@ -1,2 +1,1 @@
-rsync -avzc --no-owner --no-group --human-readable --progress --delete --exclude=.git /home/hydrogen/workspace/Space_GW/wf4ti /home/hydrogen/hh20@nrui/works/codes/gw_space/wf4ti
-/home/hydrogen/hh20@nrui/works/codes/gw_space/wf4ti
+rsync -avzc --no-owner --no-group --human-readable --progress --delete --exclude=.git --exclude='*.png' /home/hydrogen/workspace/Space_GW/wf4ti/ /home/hydrogen/hh20@nrui/works/codes/gw_space/wf4ti/

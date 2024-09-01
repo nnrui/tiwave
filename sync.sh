811a97b1535e98f42831b7e441b9ce916573bbc3
Rui Niu <nrui@mail.ustc.edu.cn>
2024-09-01 09:07:52 +0800

    modify the sync script

--- sync.sh
+++ sync.sh
@@ -1,1 +1,1 @@
-rsync -avzc --no-owner --no-group --human-readable --progress --delete --exclude=.git --exclude='*.png' --exclude='*.json' /home/hydrogen/workspace/Space_GW/wf4ti/ /home/hydrogen/hh20@nrui/works/codes/gw_space/wf4ti/
+rsync -avzc --no-owner --no-group --human-readable --progress --delete --exclude=.git --exclude='*.png' --exclude='*.json' /home/hydrogen/workspace/Space_GW/tiwave/ /home/hydrogen/hh20@nrui/works/codes/gw_space/tiwave/

57ced5007ab1d76e9305ed1298dc807034241e53
Rui Niu <nrui@mail.ustc.edu.cn>
2024-09-29 10:08:02 +0800

    add _gauss_elimination in utils.py to solve linear system of different dimension

--- tiwave/utils.py
+++ tiwave/utils.py
@@ -5,11 +5,43 @@ import taichi.math as tm
 ComplexNumber = ti.types.vector(2, ti.f64)
 
 
+@ti.func
+def _gauss_elimination(Ab: ti.template()) -> ti.template():
+    """
+    Solving a system of linear equations Ax=b using Gauss elimination. Note the loop
+    unrolling is used here, do not use this function to solve systems with large dimension.
+
+    Parameters
+    ==========
+    Ab:
+        The matrix containing the coefficinet matrix and numbers for the right hand side,
+        having the dimension of (n, n+1).
+
+    Returns
+    =======
+    x:
+        The solution of the system.
+    """
+    for i in ti.static(range(Ab.n)):
+        for j in ti.static(range(i + 1, Ab.n)):
+            scale = Ab[j, i] / Ab[i, i]
+            Ab[j, i] = 0.0
+            for k in ti.static(range(i + 1, Ab.m)):
+                Ab[j, k] -= Ab[i, k] * scale
+    # Back substitution
+    x = ti.Vector.zero(ti.f64, Ab.n)
+    for i in ti.static(range(Ab.n - 1, -1, -1)):
+        x[i] = Ab[i, Ab.m - 1]
+        for k in ti.static(range(i + 1, Ab.n)):
+            x[i] -= Ab[i, k] * x[k]
+        x[i] = x[i] / Ab[i, i]
+    return x
 
 
-
-def initialize_waveform_container_from_frequencies_array(frequencies, returned_form='polarization', include_tf=True):
-    '''
+def initialize_waveform_container_from_frequencies_array(
+    frequencies, returned_form="polarization", include_tf=True
+):
+    """
     Parparing waveform_container of ti.field from frequencies of np.array
 
     Parameters:
@@ -20,16 +52,16 @@ def initialize_waveform_container_from_frequencies_array(frequencies, returned_f
     ========
         frequency_field: ti.field
         waveform_container: ti.Struct.field({'hplus': ComplexNumber, 'hcross': ComplexNumber, 'tf': ti.f64})
-    '''
+    """
     ret_content = {}
-    if returned_form == 'polarizations':
-        ret_content.update({'hplus': ComplexNumber, 'hcross': ComplexNumber})
-    elif returned_form == 'amplitude_phase':
-        ret_content.update({'amplitude': ti.f64, 'phase': ti.f64})
+    if returned_form == "polarizations":
+        ret_content.update({"hplus": ComplexNumber, "hcross": ComplexNumber})
+    elif returned_form == "amplitude_phase":
+        ret_content.update({"amplitude": ti.f64, "phase": ti.f64})
     if include_tf:
-        ret_content.update({'tf': ti.f64})
+        ret_content.update({"tf": ti.f64})
     waveform_field = ti.Struct.field(ret_content)
-        
+
     data_length = len(frequencies)
     ti.root.dense(ti.i, data_length).place(waveform_field)
     waveform_container = waveform_field
@@ -38,5 +70,3 @@ def initialize_waveform_container_from_frequencies_array(frequencies, returned_f
     frequency_field.from_numpy(frequencies)
 
     return frequency_field, waveform_container
-
-    
